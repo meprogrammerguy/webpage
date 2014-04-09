6a70b975f8445e045d6afae3ae35d9571de7f10e
meprogrammerguy <b65535@gmail.com>
2014-04-09 06:29:02 -0500

    This is a Test

--- go.ps1
+++ go.ps1
@@ -1,5 +1,11 @@
+param(
+[string]$a
+)
 <# 
-.\go.ps1
+.\go.ps1 "Text Comment for Commit"
 #>
+git add -A
+git commit -m $a
+git push
 echo "Copying webpage to Dropbox"
 copy "$env:USERPROFILE\Documents\github\webpage\index.html" "$env:USERPROFILE\Documents\My Dropbox\ftp sync\index.html"
