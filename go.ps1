af504c598004e28f98449d5a18597a63ea254eb4
meprogrammerguy <b65535@gmail.com>
2014-04-26 11:54:52 -0500

    04/26/2014 11:54:52 page updated.

--- go.ps1
+++ go.ps1
@@ -20,4 +20,4 @@ git add -A
 git commit -m $a
 git push
 echo "Copying webpage to Dropbox"
-copy "$env:USERPROFILE\Documents\github\webpage\index.html" "$env:USERPROFILE\Documents\My Dropbox\ftp sync\index.html"
+copy "$env:USERPROFILE\Documents\github\webpage\index.html" "$env:USERPROFILE\My Dropbox\ftp sync\index.html"
