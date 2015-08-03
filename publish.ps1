761d9579f50a33538b9e7393a3ef039e9d5a8c00
meprogrammerguy <b65535@gmail.com>
2015-08-03 06:13:55 -0500

    08/03/2015 06:13:55 page updated.

--- publish.ps1
+++ publish.ps1
@@ -18,3 +18,5 @@ git commit -m $a
 git push
 echo "Copying webpage to Google Drive"
 copy "$env:USERPROFILE\Documents\github\webpage\index.html" "$env:USERPROFILE\Google Drive\ftp sync\index.html"
+echo "Copying webpage to One Drive"
+copy "$env:USERPROFILE\Documents\github\webpage\index.html" "$env:USERPROFILE\OneDrive\Documents\index.html"
