0fa0e9fe39d3174d8da1c33c6f50ee5d8463449e
meprogrammergut <b65535@gmail.com>
2015-08-04 18:35:47 -0500

    Move to OneDrive

--- publish.ps1
+++ publish.ps1
@@ -19,4 +19,4 @@ git push
 echo "Copying webpage to Google Drive"
 copy "$env:USERPROFILE\Documents\github\webpage\index.html" "$env:USERPROFILE\Google Drive\ftp sync\index.html"
 echo "Copying webpage to One Drive"
-copy "$env:USERPROFILE\Documents\github\webpage\index.html" "$env:USERPROFILE\OneDrive\Documents\index.html"
+copy "$env:USERPROFILE\Documents\github\webpage\index.html" "$env:USERPROFILE\OneDrive\Documents\ftp sync\index.html"
