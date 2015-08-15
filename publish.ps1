23696eba50fba4826f974433943f0ac2b1d8ea7a
meprogrammerguy <b65535@gmail.com>
2015-08-15 09:44:16 -0500

    Updated publish tools

--- publish.ps1
+++ publish.ps1
@@ -1,24 +1,6 @@
-param(
-[string]$a
-)
-<# 
-.\publish.ps1 "Text Comment for Commit"
-#>
-if (!$a)
-{ 
-	$a = Read-Host 'comment: or (<cr> for generic comment)'
-}
-if (!$a)
-{
-	$a = Get-Date
-	$a = $a + " page updated."
-}
+
 git add -A
-git commit -m $a
+git commit -m "Web page published from Windows box"
 git push
-<#
-echo "Copying webpage to Google Drive"
-copy "$env:USERPROFILE\Documents\github\webpage\index.html" "$env:USERPROFILE\Google Drive\ftp sync\index.html"
-#>
 echo "Copying webpage to One Drive"
 copy "$env:USERPROFILE\Documents\github\webpage\index.html" "$env:USERPROFILE\OneDrive\Documents\ftp sync\index.html"
--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git add -A
-git commit -m "Web page updated from Mint Linux box"
+git commit -m "Web page published from Mint Linux box"
 git push
 echo "Copying webpage to One Drive"
 cp index.html "/home/jsmith/OneDrive/Documents/ftp sync/"
