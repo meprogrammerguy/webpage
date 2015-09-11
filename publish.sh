d710aee82a66a57caab6952ad0ff8bd21a3598e4
meprogrammerguy <b65535@gmail.com>
2015-09-10 19:41:08 -0500

    Web page published from linux-home box

--- publish.sh
+++ publish.sh
@@ -4,4 +4,4 @@ git add -A
 git commit -m "Web page published from linux-home box"
 git push
 echo "Copying to OneDrive from Linux mint"
-cp index.html "smb://win10-hd-home/users/jsmith/OneDrive/Documents/ftp sync/"
+cp -r index.html "smb://win10-hd-home/users/jsmith/OneDrive/Documents/ftp sync/"
