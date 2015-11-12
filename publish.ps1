38b15fd996c35cb76b42ae4fd499d1505d9b05c7
meprogrammerguy <b65535@gmail.com>
2015-11-12 09:38:15 -0600

    Web page published from Windows box

--- publish.ps1
+++ publish.ps1
@@ -3,4 +3,4 @@ git add -A
 git commit -m "Web page published from Windows box"
 git push
 echo "Copying webpage to One Drive"
-copy "$env:USERPROFILE\Documents\github\webpage\index.html" "$env:USERPROFILE\OneDrive\Documents\ftp sync\index.html"
+copy "C:\Users\jsmith.al-jsmith-sp3\OneDrive\Documents\ftp sync\Documents\github\webpage\index.html" "C:\Users\jsmith.al-jsmith-sp3\OneDrive\Documents\ftp sync\OneDrive\Documents\ftp sync\index.html"
