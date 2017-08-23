9d303349d4fe107338697e93152337778dbab983
meprogrammerguy <b65535@gmail.com>
2017-08-23 08:33:22 -0500

    Web page published from Windows box

--- publish.ps1
+++ publish.ps1
@@ -1,3 +1,4 @@
+$filelocation = $PSScriptRoot + '\index(1).html'
 if (Test-Path -path $filelocation)
 {
 	Remove-Item index.html
@@ -7,6 +8,5 @@ git add -A
 git commit -m "Web page published from Windows box"
 git push
 echo "Copying webpage to One Drive"
-$filelocation = $PSScriptRoot + '\index(1).html'
 copy "index.html" "$env:USERPROFILE\OneDrive\Documents\ftp sync\index.html"
 
