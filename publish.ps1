0ba6da8885c91130ecc517faef8a8b7b1a308cf4
meprogrammerguy <b65535@gmail.com>
2017-08-23 08:11:02 -0500

    Web page published from Windows box

--- publish.ps1
+++ publish.ps1
@@ -3,5 +3,10 @@ git add -A
 git commit -m "Web page published from Windows box"
 git push
 echo "Copying webpage to One Drive"
+if (Test-Path index(1).html)
+{
+	Remove-Item index.html
+	Rename-Item index(1).html -NewName index.html
+}
 copy "index.html" "$env:USERPROFILE\OneDrive\Documents\ftp sync\index.html"
 
