0d0ab17fb10d55ba331ea598d68c8a1006d78ddd
meprogrammerguy <b65535@gmail.com>
2017-10-11 08:59:58 -0500

    Web page published from linux-home box

--- publish.sh
+++ publish.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 
-$FILE = "index (1).html"
+$FILE = "index(1).html"
 if [ -f $FILE ]; then
    rm -f index.html
-   mv $FILE index.html
+   mv "$FILE" "index.html"
 fi
 git add -A
 git commit -m "Web page published from linux-home box"
