1c088f674e2b77ec8f2e8c64112d5f436e8cca90
meprogrammerguy <b65535@gmail.com>
2017-10-11 09:07:03 -0500

    Web page published from linux-home box

--- publish.sh
+++ publish.sh
@@ -1,12 +1,7 @@
 #!/bin/zsh
 
-$FILE = "/home/jsmith/git/webpage/index(1).html"
-if [ -f $FILE ]; then
-   rm -f index.html
-   mv "$FILE" "/home/jsmith/git/webpage/index.html"
-fi
 git add -A
 git commit -m "Web page published from linux-home box"
 git push
-echo "Copying to OneDrive from Linux mint"
+echo "Copying to OneDrive from Linux ubuntu"
 cp -f index.html "/home/jsmith/OneDrive/Documents/ftp sync"
