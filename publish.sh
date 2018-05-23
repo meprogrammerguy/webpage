7d723cfc650800874b0ecb18ceb6fcc19b478b3a
meprogrammerguy <b65535@gmail.com>
2018-05-23 15:13:57 -0500

    Web page published from linux-home box

--- publish.sh
+++ publish.sh
@@ -6,6 +6,8 @@ git push
 echo "Cleaning up old files on OneDrive in LocalWeb"
 rm -rf "/home/jsmith/OneDrive/Documents/LocalWeb/*"
 echo "Copying to OneDrive from Linux ubuntu"
+ls -R > files.txt
+cp -f "files.txt" "/home/jsmith/OneDrive/Documents/LocalWeb"
 cp -f "index.html" "/home/jsmith/OneDrive/Documents/LocalWeb"
 cp -r "templates/" "/home/jsmith/OneDrive/Documents/LocalWeb"
 cp -r "style/" "/home/jsmith/OneDrive/Documents/LocalWeb"
