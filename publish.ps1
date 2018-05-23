b0ca6c61ae20c58b295a1810091721765ec862e0
meprogrammerguy <b65535@gmail.com>
2018-05-23 13:44:50 -0500

    started my new page

--- publish.ps1
+++ publish.ps1
@@ -1,12 +1,6 @@
-$filelocation = $PSScriptRoot + '\index(1).html'
-if (Test-Path -path $filelocation)
-{
-	Remove-Item index.html
-	Rename-Item $filelocation -NewName index.html
-}
 git add -A
 git commit -m "Web page published from Windows box"
 git push
 echo "Copying webpage to One Drive"
-copy "index.html" "$env:USERPROFILE\OneDrive\Documents\ftp sync\index.html"
+copy "index.html" "$env:USERPROFILE\OneDrive\Documents\LocalWeb\index.html"
 
--- publish.sh
+++ publish.sh
@@ -4,4 +4,4 @@ git add -A
 git commit -m "Web page published from linux-home box"
 git push
 echo "Copying to OneDrive from Linux ubuntu"
-cp -f "index.html" "/home/jsmith/OneDrive/Documents/ftp sync"
+cp -f "index.html" "/home/jsmith/OneDrive/Documents/LocalWeb"
