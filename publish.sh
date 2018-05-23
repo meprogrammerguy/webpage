c40713d3ec1a21236f479eb8bf2d364f9806a587
meprogrammerguy <b65535@gmail.com>
2018-05-23 13:48:13 -0500

    Web page published from linux-home box

--- publish.sh
+++ publish.sh
@@ -5,3 +5,5 @@ git commit -m "Web page published from linux-home box"
 git push
 echo "Copying to OneDrive from Linux ubuntu"
 cp -f "index.html" "/home/jsmith/OneDrive/Documents/LocalWeb"
+cp -r "templates/" "/home/jsmith/OneDrive/Documents/LocalWeb"
+cp -r "style/" "/home/jsmith/OneDrive/Documents/LocalWeb"
