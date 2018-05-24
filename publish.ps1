61434abb977b8595a63aba853a44c13bc6797a0f
meprogrammerguy <b65535@gmail.com>
2018-05-24 14:56:44 -0500

    Web page published from linux-home box

--- publish.ps1
+++
@@ -1,8 +0,0 @@
-git add -A
-git commit -m "Web page published from Windows box"
-git push
-echo "Copying webpage to One Drive"
-copy "index.html" "$env:USERPROFILE\OneDrive\Documents\LocalWeb\index.html"
-copy "templates/" "/home/jsmith/OneDrive/Documents/LocalWeb"
-copy "style/" "/home/jsmith/OneDrive/Documents/LocalWeb"
-
